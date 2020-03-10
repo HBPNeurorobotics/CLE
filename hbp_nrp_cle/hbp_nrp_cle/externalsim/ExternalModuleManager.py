b3a5983d7b8ea3aeef72b4a636d5b20f3c97c0f4
Michael Zechmair <michael.zechmair@in.tum.de>
2020-03-10 07:20:28 +0000

    [NRRPLT-7823] Refactored ExternalModuleManager to reduce code duplication

--- hbp_nrp_cle/hbp_nrp_cle/externalsim/ExternalModuleManager.py
+++ hbp_nrp_cle/hbp_nrp_cle/externalsim/ExternalModuleManager.py
@@ -10,7 +10,6 @@ import re
 import rosservice
 from hbp_nrp_cle.externalsim.ExternalModule import ExternalModule
 
-
 class ExternalModuleManager(object):
     """
     This class automatically detects the external modules searching the ROS
@@ -24,13 +23,13 @@ class ExternalModuleManager(object):
     def __init__(self):
         self.module_names = []
         for service in rosservice.get_service_list():
-            m = re.match(r"/emi/.*/initialize", str(service))
+            m = re.match(r"/emi/(\w+)_module/initialize", str(service))
             if m:
-                module_name = m.group(0)[5:-18]
+                module_name = m.group(1)
                 self.module_names.append(module_name)
 
         self.ema = []
-        if len(self.module_names) is not 0:
+        if self.module_names:
             with concurrent.futures.ThreadPoolExecutor(max_workers=max(len(self.module_names), 1))\
                     as executor:
                 future_results = [executor.submit(ExternalModule, x) for x in self.module_names]
@@ -38,37 +37,33 @@ class ExternalModuleManager(object):
                 for future in future_results:
                     self.ema.append(future.result())
 
-    def initialize(self):
+    def exec_module_call(self, function):
         """
-        This method is used to run all initialize methods served at each external models at once.
+        Creates a thread for every module and executes the requested function in it
+        :param function: Function to execute on every module
         """
-        if len(self.module_names) is not 0:
+        if self.module_names:
             with concurrent.futures.ThreadPoolExecutor(max_workers=len(self.ema)) as executor:
-                future_results = [executor.submit(x.initialize) for x in self.ema]
+                future_results = [executor.submit(function, x) for x in self.ema]
                 concurrent.futures.wait(future_results)
                 for future in future_results:
                     while not future.result().done():
                         pass
+
+    def initialize(self):
+        """
+        This method is used to run all initialize methods served at each external models at once.
+        """
+        self.exec_module_call(ExternalModule.initialize)
 
     def run_step(self):
         """
         This method is used to run all run_step methods served at each external models at once.
         """
-        if len(self.module_names) is not 0:
-            with concurrent.futures.ThreadPoolExecutor(max_workers=len(self.ema)) as executor:
-                future_results = [executor.submit(x.run_step) for x in self.ema]
-                concurrent.futures.wait(future_results)
-                for future in future_results:
-                    while not future.result().done():
-                        pass
+        self.exec_module_call(ExternalModule.run_step)
 
     def shutdown(self):
         """
         This method is used to run all shutdown methods served at each external models at once.
         """
-        with concurrent.futures.ThreadPoolExecutor(max_workers=len(self.ema)) as executor:
-            future_results = [executor.submit(x.shutdown) for x in self.ema]
-            concurrent.futures.wait(future_results)
-            for future in future_results:
-                while not future.result().done():
-                    pass
+        self.exec_module_call(ExternalModule.shutdown)
